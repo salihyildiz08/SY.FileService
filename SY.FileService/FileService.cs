13f800b7f2f5c59353a06619c83a7422fceaa990
Salih YILDIZ <yldzsalih27@gmail.com>
2024-09-03 13:01:29 +0300

    Add project files.

---
+++ SY.FileService/FileService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace SY.FileService
+{
+    public static class FileService
+    {
+        public static string FileSaveToServer(IFormFile file, string filePath)
+        {
+            string fileName = string.Join(".", DateTime.Now.ToFileTime().ToString(), file.FileName);
+            string path = filePath + fileName;
+            using (var stream = File.Create(path))
+            {
+                file.CopyTo(stream);
+            }
+            return fileName;
+        }
+
+        public static void FileDeleteToServer(string path)
+        {
+            try
+            {
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
---
+++ SY.FileService/README.md
@@ -0,0 +1,19 @@
+﻿# Dependency
+This library was created by .Net 5.0
+
+# Install
+dotnet add package SY.FileService
+
+# Usage
+If you want to save file in server
+
+``` string filePath = "./Files/";
+
+string fileName = FileService.FileSaveToServer(file,filePath);
+
+-- If you want to delete file in server
+
+string path = "./Files/" + "FileName";
+
+FileService.FileDeleteToServer(path);
+
